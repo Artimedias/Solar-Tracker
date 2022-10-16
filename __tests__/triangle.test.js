c4c8186ade1c58b26c3aa4debb9f7ea556dc4986
Artimedias <brettroper22@gmail.com>
2022-10-16 02:43:17 -0700

    Test two passed, the script can now test what the users age is on different planets

--- __tests__/triangle.test.js
+++ __tests__/triangle.test.js
@@ -10,3 +10,21 @@ let Jermey;
     expect(Jermey.checktype(Jermey.user)[1]).toEqual("male");
   });
 });
+
+describe('ageConvert', () => {
+  let Jane;
+  beforeEach(() => {
+    Jane = new user(20, "female");
+  });
+  test('Should be able to respond with the age on all the different planets', () => {
+    expect(Jane.ageConvert(Jane.user)[0]).toEqual("83.20");
+    expect(Jane.ageConvert(Jane.user)[1]).toEqual("32.40");
+    expect(Jane.ageConvert(Jane.user)[2]).toEqual("20.00");
+    expect(Jane.ageConvert(Jane.user)[3]).toEqual("10.60");
+    expect(Jane.ageConvert(Jane.user)[4]).toEqual("1.68");
+    expect(Jane.ageConvert(Jane.user)[5]).toEqual("0.68");
+    expect(Jane.ageConvert(Jane.user)[6]).toEqual("0.24");
+    expect(Jane.ageConvert(Jane.user)[7]).toEqual("0.12");
+  });
+});
+
--- js/age.js
+++ js/age.js
@@ -8,5 +8,18 @@ checktype() {
     let returnArray = [this.age, this.sex];
     return returnArray;
 }
+
+ageConvert() {
+  let planets = [4.16, 1.62, 1, .53, .084, .034, .012, .006]
+  let newAge = [];
+
+  for(let i = 0; i < 8; i++)
+  {
+    newAge.push((planets[i] * this.age).toFixed(2))
+  }
+
+  return newAge;
 }
-  
+}
+
+//Mercury = 4.16, Venus = 1.62, Mars = .53, Jupiter = .084, Saturn = .034, Uranus = .012, Neptune = .006
