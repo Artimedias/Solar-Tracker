99dbc1583efcce3e4ac0fe49d7325ecf6943e15b
Artimedias <brettroper22@gmail.com>
2022-10-16 08:21:00 -0700

    Set up final test for detirming the life left on various planets.

--- __tests__/triangle.test.js
+++ __tests__/triangle.test.js
@@ -28,3 +28,32 @@ describe('ageConvert', () => {
   });
 });
 
+describe('left', () => {
+  let James;
+  let Jessica;
+  beforeEach(() => {
+    James = new user(60, "male");
+    Jessica = new user(120, "female");
+  });
+  test('Should respond with the remaining life left for a 60 y/o male', () => {
+    expect(James.lifeLeft(James.user)[0]).toEqual("66.97");
+    expect(James.lifeLeft(James.user)[1]).toEqual("26.08");
+    expect(James.lifeLeft(James.user)[2]).toEqual("16.01");
+    expect(James.lifeLeft(James.user)[3]).toEqual("08.53");
+    expect(James.lifeLeft(James.user)[4]).toEqual("01.35");
+    expect(James.lifeLeft(James.user)[5]).toEqual("00.54");
+    expect(James.lifeLeft(James.user)[6]).toEqual("00.19");
+    expect(James.lifeLeft(James.user)[7]).toEqual("00.09");
+  });
+  test('Should respond how much over the average lifespan a 120 y/o female is', () => {
+    expect(Jessica.lifeLeft(Jessica.user)[0]).toEqual("161.83");
+    expect(Jessica.lifeLeft(Jessica.user)[1]).toEqual("63.02");
+    expect(Jessica.lifeLeft(Jessica.user)[2]).toEqual("38.90");
+    expect(Jessica.lifeLeft(Jessica.user)[3]).toEqual("20.62");
+    expect(Jessica.lifeLeft(Jessica.user)[4]).toEqual("3.26");
+    expect(Jessica.lifeLeft(Jessica.user)[5]).toEqual("1.32");
+    expect(Jessica.lifeLeft(Jessica.user)[6]).toEqual("0.47");
+    expect(Jessica.lifeLeft(Jessica.user)[7]).toEqual("0.23");
+  });
+});
+
